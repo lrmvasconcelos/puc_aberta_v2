a58cfa37149a659fc0f3226a7e963943bc5b8681
lrmv <lrmvasconcelos@sga.pucminas.br>
2018-04-12 16:33:45 -0300

    Corrige método de mostrar cursos dentro do app pela WebView

--- PUCAberta/app/src/main/java/pucaberta/pucminas/com/ui/fragment/CursosFragment.java
+++ PUCAberta/app/src/main/java/pucaberta/pucminas/com/ui/fragment/CursosFragment.java
@@ -1,5 +1,6 @@
 package pucaberta.pucminas.com.ui.fragment;
 
+import android.annotation.SuppressLint;
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -7,11 +8,12 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
 
 import pucaberta.pucminas.com.R;
-import pucaberta.pucminas.com.base.BaseFragment;
 import pucaberta.pucminas.com.base.BaseFragmentViewModel;
 import pucaberta.pucminas.com.databinding.FragmentCursosBinding;
 import pucaberta.pucminas.com.helper.Utils;
@@ -30,12 +32,13 @@ public class CursosFragment extends BaseFragmentViewModel<FragmentCursosBinding,
         return new CursosFragment();
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_cursos, container, false);
 
-        if(mViewModel == null){
+        if (mViewModel == null) {
             mViewModel = new CursosViewModel(this);
         }
 
@@ -51,16 +54,27 @@ public class CursosFragment extends BaseFragmentViewModel<FragmentCursosBinding,
     }
 
     private void layoutVisibility() {
+
         if (Utils.isOnline()) {
-            Toast.makeText(getActivity(), R.string.conexao_internet_habilitada, Toast.LENGTH_SHORT).show();
 
             mBinding.webView.setVisibility(View.VISIBLE);
-            mBinding.webView.loadUrl("http://www.pucminas.br/Graduacao/Paginas/default.aspx");
-            mBinding.relativeLayout.setVisibility(View.GONE);
 
+            mBinding.webView.setWebChromeClient(new WebChromeClient() {
+                public void onProgressChanged(WebView view, int progress) {
+                    // Activities and WebViews measure progress with different scales.
+                    // The progress meter will automatically disappear when we reach 100%
+
+                }
+            });
+            mBinding.webView.setWebViewClient(new WebViewClient() {
+                public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                    Toast.makeText(getContext(), "Oh no! " + description, Toast.LENGTH_SHORT).show();
+                }
+            });
+
+            mBinding.webView.loadUrl("http://www.pucminas.br/Graduacao/Paginas/default.aspx");
         } else {
             Toast.makeText(getActivity(), R.string.sem_conexao_internet, Toast.LENGTH_SHORT).show();
-
             mBinding.webView.setVisibility(View.GONE);
             mBinding.relativeLayout.setVisibility(View.VISIBLE);
         }
