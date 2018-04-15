da1a9bb2f61ab5a716e1ebef41e5a8f84d8a3323
lrmv <lrmvasconcelos@sga.pucminas.br>
2018-04-15 14:33:46 -0300

    Implementa tela de menu

--- PUCAberta/app/src/main/java/pucaberta/pucminas/com/ui/fragment/MenuFragment.java
+++ PUCAberta/app/src/main/java/pucaberta/pucminas/com/ui/fragment/MenuFragment.java
@@ -10,13 +10,16 @@ import android.view.ViewGroup;
 
 import pucaberta.pucminas.com.R;
 import pucaberta.pucminas.com.base.BaseFragmentViewModel;
+import pucaberta.pucminas.com.base.BaseViewModel;
+import pucaberta.pucminas.com.databinding.FragmentMenuBinding;
+import pucaberta.pucminas.com.viewmodel.MenuViewModel;
 
 /**
  * Created by lucas on 09/04/2018.
  * Update at 09/04/2018
  */
 
-public class MenuFragment extends BaseFragmentViewModel {
+public class MenuFragment extends BaseFragmentViewModel<FragmentMenuBinding, MenuViewModel> implements View.OnClickListener{
 
     public static MenuFragment newInstance() {
 
@@ -27,6 +30,21 @@ public class MenuFragment extends BaseFragmentViewModel {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_menu, container, false);
+        if(mViewModel == null){
+            mViewModel = new MenuViewModel(this);
+        }
         return mBinding.getRoot();
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.log_out:
+
+                break;
+
+            default:
+                break;
+        }
     }
 }
---
+++ PUCAberta/app/src/main/java/pucaberta/pucminas/com/viewmodel/MenuViewModel.java
@@ -0,0 +1,15 @@
+package pucaberta.pucminas.com.viewmodel;
+
+import pucaberta.pucminas.com.base.BaseViewModel;
+import pucaberta.pucminas.com.interfaces.CallbackBasicViewModel;
+
+/**
+ * Created by lucas on 15/04/2018.
+ * Update at 15/04/2018
+ */
+
+public class MenuViewModel extends BaseViewModel {
+    public MenuViewModel(CallbackBasicViewModel callback) {
+        super(callback);
+    }
+}
