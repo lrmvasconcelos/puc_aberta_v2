6149471089802f58a18e9d8f2082fd9710749118
lrmv <lrmvasconcelos@sga.pucminas.br>
2018-04-16 07:53:13 -0300

    Implementa método de buscar localização do usuário

---
+++ PUCAberta/app/src/main/java/pucaberta/pucminas/com/interfaces/OnItemListener.java
@@ -0,0 +1,12 @@
+package pucaberta.pucminas.com.interfaces;
+
+/**
+ * Created by lucas on 16/04/2018.
+ * Update at 16/04/2018
+ */
+
+public interface OnItemListener {
+
+    void lectureClicked(String tittle);
+
+}
